5082a91c847399e36a43a9bec4dea558ac41d289
Aomeli <43156436+Aomeli@users.noreply.github.com>
2018-09-20 19:40:42 -0500

    Add files via upload

---
+++ Valeria/src/abstraccion/Circulo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion;
+
+/**
+ *
+ * @author T-101
+ */
+public class Circulo {
+float radio;
+float area;
+public Circulo(){
+    area=10;
+}
+
+void calcularArea(){
+   area=3.1416f*(radio*radio); 
+}
+}
